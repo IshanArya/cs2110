27c12d178399ef852f13581937052463224d4872
Ishan Arya <aryaishn@gmail.com>
2019-03-13 02:38:53 -0400

    Just have to implement save on hw8

--- hw8/todo.c
+++ hw8/todo.c
@@ -3,6 +3,9 @@
 
 char getUserOption(void);
 void printToDoList(void);
+char addItem(void);
+void markCompleted(void);
+int removeCompleted(void);
 
 int main(int argc, char *argv[]) {
 
@@ -28,10 +31,15 @@ int main(int argc, char *argv[]) {
                 printToDoList();
                 break;
             case '2':
+                if(addItem() == 0) {
+                    printf("%s", LIST_MAX);
+                }
                 break;
             case '3':
+                markCompleted();
                 break;
             case '4':
+                printf("Success! %d items removed!\n", removeCompleted());
                 break;
             case '5':
                 break;
@@ -86,5 +94,46 @@ void printToDoList(void) {
         printf("Description: %s\n", currentItem.description);
         printf("%s", LINE_BREAK);
     }
+}
+
+char addItem(void) {
+    printf("%s", ADD_TO_LIST_HEADER);
+    if(Todo_list_length >= TODO_LIST_MAX) {
+        return 0;
+    }
+
+    printf("%s", ADD_TO_LIST);
+    return read_todo_list_item(stdin);
+}
+
+void markCompleted(void) {
+    printf("%s", MARK_ITEM_COMPLETED_HEADER);
+    printf("%s", MARK_ITEM_USER_INPUT);
+
+    printf("%s", GRAB_INPUT);
+
+    char input[10];
+    if (fgets(input, 10, stdin) == NULL) {
+    }
+
+    int index = atoi(input);
+
+    Todo_list[index].is_done = 1;
+}
+
+int removeCompleted(void) {
+    printf("%s", REMOVE_ITEM_HEADER);
+
+    int numberRemoved = 0;
+    for(int i = 0; i < Todo_list_length; i++) {
+        if(Todo_list[i].is_done == 1) {
+            numberRemoved++;
+        } else {
+            Todo_list[i - numberRemoved] = Todo_list[i];
+        }
+    }
+
+    Todo_list_length -= numberRemoved;
+    return numberRemoved;
 }
 
